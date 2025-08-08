9bed12c900daee7c4597e7a4de17ed9bbcc419b3
blurbeast <leumasre@gmail.com>
2025-08-08 12:15:58 +0100

    KK

--- src/erc20.rs
+++ src/erc20.rs
@@ -29,8 +29,20 @@ impl ERC20  {
     }
     fn approve(&mut self, spender: Address, amount: U256) -> ArbResult {
         let from: Address = self.vm().msg_sender();
-        self.allowance.setter(spender).setter(from).set(amount);
+        self.allowance.setter(from).setter(spender).set(amount);
         Ok(vec![true.into()])
+    }
+
+    fn transfer_from(&mut self, owner: Address, to: Address, value: U256) -> Result<bool, Vec<u8>> {
+        let from = self.vm().msg_sender();
+        let allowance = self.allowance.get(owner).get(from);
+        assert!(allowance > U256::from(0), "insufficient allowance");
+        assert!(value <= allowance, "insufficient allowance");
+        self.allowance.setter(from).setter(to).set(allowance-value);
+        let owner_balance = self.balance.get(owner);
+        self._transfer(owner, owner_balance, to, value);
+
+        Ok(true)
     }
     fn transfer(&mut self, to: Address, value: U256) -> Result<bool, Vec<u8>> {
         //remove from the msg to address to
@@ -117,6 +129,14 @@ mod test {
         let (mut erc20, owner, vm) = create_erc20_instance();
         let to: Address = Address::from(U160::from(0x0000000000000000000000000000000000000001));
 
-        erc20.approve(owner, U256::from(100)).unwrap();
+        erc20.approve(to, U256::from(100)).unwrap();
+        assert_eq!(erc20.allowance.get(owner).get(to), U256::from(100));
+
+        vm.set_sender(to);
+        erc20.transfer_from(owner, vm.contract_address(), U256::from(100)).unwrap();
+        assert_eq!(erc20.balance.get(owner), U256::from(999_999_900));
+        assert_eq!(erc20.balance.get(vm.contract_address()), U256::from(100));
+
+        assert_eq!(erc20.balance.get(to), U256::from(0));
     }
 }
